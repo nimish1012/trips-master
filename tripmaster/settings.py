efbb59c5ca5e74a138d7ca3fe6cdf553b2ec29a5
nimish1012 <nimishgupta1012@gmail.com>
2023-07-27 12:09:10 +0530

    travalouge_page02

--- tripmaster/settings.py
+++ tripmaster/settings.py
@@ -77,17 +77,23 @@ WSGI_APPLICATION = 'tripmaster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'trip_app',
-       'USER': 'root',
-       'PASSWORD': '',
-       'HOST': '127.0.0.1',
-       'PORT': '3306',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'trip_app',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',
+#    }
+# }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com' # since you are using a gmail account
